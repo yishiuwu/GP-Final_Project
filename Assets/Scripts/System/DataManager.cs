85e3b04e26a0a4f37f872c5017e66da193c4dec8
yishiuwu <94129760+yishiuwu@users.noreply.github.com>
2023-12-24 01:29:30 +0800

    data manager init

---
+++ Assets/Scripts/System/DataManager.cs
@@ -0,0 +1,15 @@
+static public class DataManager 
+{
+    // Initialize data
+    static public bool Init() {
+
+        return true;
+    }
+
+    // Load data in to game
+    static public bool Load(string key) {
+
+        return true;
+    }
+
+}
