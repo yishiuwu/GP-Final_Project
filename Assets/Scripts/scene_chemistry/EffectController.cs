4495b4e1a7f0fc3ff55e37e2a34df3e4cea5aa55
Tracy Chung <tracy9207ok52027@gmail.com>
2023-12-25 23:48:57 +0800

    try switch partical effect

---
+++ Assets/Scripts/scene_chemistry/EffectController.cs
@@ -0,0 +1,109 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class EffectController : MonoBehaviour
+{
+    public GameObject solidAcid;
+    public GameObject solidAlkali;
+    public GameObject liquadAcid;
+    public GameObject liquadAlkali;
+
+    public event Action DisableParticalEffect;
+    public event Action EnableParticalEffect;
+
+    public static EffectController Instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        solidAcid.SetActive(false);
+        solidAlkali.SetActive(false);
+        liquadAcid.SetActive(false);
+        liquadAlkali.SetActive(false);
+
+        DisableParticalEffect += ()=>{
+            solidAcid.SetActive(false);
+            solidAlkali.SetActive(false);
+            liquadAcid.SetActive(false);
+            liquadAlkali.SetActive(false);
+            Debug.Log("remove effect");
+        };
+
+        EnableParticalEffect += ()=>{
+            if(!StatusSystem.Instance.isMelted){
+                switch(StatusSystem.Instance.ph){
+                    case "acid":
+                        solidAcid.SetActive(true);
+                        solidAlkali.SetActive(false);
+                        liquadAcid.SetActive(false);
+                        liquadAlkali.SetActive(false);
+                        Debug.Log("get solid acid effect");
+                        break;
+                    case "alkali":
+                        solidAcid.SetActive(false);
+                        solidAlkali.SetActive(true);
+                        liquadAcid.SetActive(false);
+                        liquadAlkali.SetActive(false);
+                        Debug.Log("get solid alkali effect");
+                        break;
+                }
+            }
+            else if(StatusSystem.Instance.isMelted){
+                switch(StatusSystem.Instance.ph){
+                    case "acid":
+                        solidAcid.SetActive(false);
+                        solidAlkali.SetActive(false);
+                        liquadAcid.SetActive(true);
+                        liquadAlkali.SetActive(false);
+                        Debug.Log("get melt acid effect");
+                        break;
+                    case "alkali":
+                        solidAcid.SetActive(false);
+                        solidAlkali.SetActive(false);
+                        liquadAcid.SetActive(false);
+                        liquadAlkali.SetActive(true);
+                        Debug.Log("get melt alkali effect");
+                        break;
+                }
+            }
+        };
+
+        Instance = this;
+    }
+
+    public void getPartical(){
+        EnableParticalEffect?.Invoke();
+    }
+
+    public void removePartical(){
+        DisableParticalEffect?.Invoke();
+    }
+
+    // [TODO] before switch status
+    // EffectController.Instance.removePartical();
+    // [TODO] after switch status
+    // EffectController.Instance.getPartical(); 
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if(StatusSystem.Instance.ph == "neutral"){
+        //     solidAcid.SetActive(false);
+        //     solidAlkali.SetActive(false);
+        // }
+        // else if(StatusSystem.Instance.ph == "acid"){
+        //     if(!StatusSystem.Instance.isMelted){
+        //         solidAcid.SetActive(true);
+        //         solidAlkali.SetActive(false);
+        //     }
+        // }
+        // else if(StatusSystem.Instance.ph == "alkali"){
+        //     if(!StatusSystem.Instance.isMelted){
+        //         solidAcid.SetActive(false);
+        //         solidAlkali.SetActive(true);
+        //     }
+        // }
+    }
+}
